a10f03aede34224fe731f9e565da6a270c1b52dc
MB20 JCLiu <JCLiu@nuvoton.com>
2024-03-21 13:27:44 +0800

    1,upated gui. 2,add image sample. 3,init update rtc.

---
+++ NUVOTON_ISP_COMMAND/NUVOTON_ISP_COMMAND/nuvoton_magic_board_util/resource.h
@@ -0,0 +1,106 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by nuvotonmagicboardutil.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_NUVOTON_MAGIC_BOARD_UTIL_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     133
+#define IDC_BUTTON1                     1000
+#define IDC_EDIT1                       1001
+#define IDC_BUTTON2                     1002
+#define IDC_BUTTON3                     1003
+#define IDC_CHECK1                      1004
+#define IDC_CHECK2                      1005
+#define IDC_CHECK3                      1006
+#define IDC_CHECK4                      1007
+#define IDC_CHECK5                      1008
+#define IDC_CHECK6                      1009
+#define IDC_CHECK7                      1010
+#define IDC_CHECK8                      1011
+#define IDC_CHECK9                      1012
+#define IDC_CHECK10                     1013
+#define IDC_CHECK11                     1014
+#define IDC_CHECK12                     1015
+#define IDC_CHECK13                     1016
+#define IDC_CHECK14                     1017
+#define IDC_CHECK15                     1018
+#define IDC_CHECK16                     1019
+#define IDC_CHECK17                     1020
+#define IDC_CHECK18                     1021
+#define IDC_CHECK19                     1022
+#define IDC_CHECK20                     1023
+#define IDC_CHECK21                     1024
+#define IDC_CHECK22                     1025
+#define IDC_CHECK23                     1026
+#define IDC_CHECK24                     1027
+#define IDC_CHECK25                     1028
+#define IDC_CHECK26                     1029
+#define IDC_CHECK27                     1030
+#define IDC_CHECK28                     1031
+#define IDC_CHECK29                     1032
+#define IDC_CHECK30                     1033
+#define IDC_CHECK31                     1034
+#define IDC_CHECK32                     1035
+#define IDC_CHECK33                     1036
+#define IDC_CHECK34                     1037
+#define IDC_CHECK35                     1038
+#define IDC_CHECK36                     1039
+#define IDC_CHECK37                     1040
+#define IDC_CHECK38                     1041
+#define IDC_CHECK39                     1042
+#define IDC_CHECK40                     1043
+#define IDC_CHECK41                     1044
+#define IDC_CHECK42                     1045
+#define IDC_CHECK43                     1046
+#define IDC_CHECK44                     1047
+#define IDC_CHECK45                     1048
+#define IDC_CHECK46                     1049
+#define IDC_CHECK47                     1050
+#define IDC_CHECK48                     1051
+#define IDC_CHECK49                     1052
+#define IDC_CHECK50                     1053
+#define IDC_CHECK51                     1054
+#define IDC_CHECK52                     1055
+#define IDC_CHECK53                     1056
+#define IDC_CHECK54                     1057
+#define IDC_CHECK55                     1058
+#define IDC_CHECK56                     1059
+#define IDC_CHECK57                     1060
+#define IDC_CHECK58                     1061
+#define IDC_CHECK59                     1062
+#define IDC_CHECK60                     1063
+#define IDC_CHECK61                     1064
+#define IDC_CHECK62                     1065
+#define IDC_CHECK63                     1066
+#define IDC_CHECK64                     1067
+#define IDC_BUTTON4                     1068
+#define IDC_BUTTON5                     1069
+#define IDC_BUTTON6                     1070
+#define IDC_BUTTON7                     1071
+#define IDC_STATICBOOT                  1072
+#define IDC_BUTTON8                     1073
+#define IDC_STATICVERSION               1074
+#define IDC_STATICISP                   1075
+#define IDC_BUTTON9                     1076
+#define IDC_BUTTON10                    1077
+#define IDC_BUTTON11                    1078
+#define IDC_BUTTON12                    1079
+#define IDC_BUTTON13                    1080
+#define IDC_BUTTON14                    1081
+#define IDC_BUTTON15                    1082
+#define IDC_BUTTON16                    1083
+#define IDC_BUTTON17                    1084
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1078
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
